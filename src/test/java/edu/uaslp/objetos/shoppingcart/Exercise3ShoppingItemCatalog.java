6cf4aedc98ca651bfd99954debae9a8980d0683a
Mario Velez <a156772@alumnos.uaslp.mx>
2023-05-08 09:58:34 -0600

    Parcial 3 - respuestas_parte2

--- src/test/java/edu/uaslp/objetos/shoppingcart/Exercise3ShoppingItemCatalog.java
+++ src/test/java/edu/uaslp/objetos/shoppingcart/Exercise3ShoppingItemCatalog.java
@@ -1,16 +1,36 @@
 package edu.uaslp.objetos.shoppingcart;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.when;
 
 public class Exercise3ShoppingItemCatalog {
 
 
     @Test
     public void givenAValidCode_whenGetItem_thenShoppingItemIsReturned(){
+        // Given:
+        ShoppingItemCatalog shoppingItemCatalog = Mockito.mock(ShoppingItemCatalog.class);
+        ShoppingCart shoppingCart = new ShoppingCart(shoppingItemCatalog);
+        String itemCode = "9785";
+        ShoppingItem shoppingItem = shoppingItemCatalog.getItem(itemCode);
+
+        Assertions.assertThat(shoppingItem.getCode()).isEqualTo(null);
+
     }
 
     @Test
     public void givenANotValidCode_whenGetItem_thenNullIsReturned(){
+        // Given:
+        ShoppingItemCatalog shoppingItemCatalog = Mockito.mock(ShoppingItemCatalog.class);
+        ShoppingCart shoppingCart = new ShoppingCart(shoppingItemCatalog);
+        String itemCode = "ABC1000";
+        ShoppingItem shoppingItem = shoppingItemCatalog.getItem(itemCode);
+        Assertions.assertThat(shoppingItem.getCode()).isEqualTo("ABC1000");
+
+
 
     }
 }
